bd20cdf7df5f675d9dec36a03a18d4ffc4b2ec3d
bmunyoki <munyokibenjamin@gmail.com>
2018-07-22 18:16:41 +0200

    Done

--- app/Http/Controllers/Backend/RoomsController.php
+++ app/Http/Controllers/Backend/RoomsController.php
@@ -10,7 +10,7 @@ use App\User;
 class RoomsController extends Controller{
     //Get allocated rooms
     public function getAllocatedRooms(){
-    	$students = User::whereNotNull('room')->paginate(10);
+    	$students = User::whereNotIn('email', ['bmunyoki@africa.cmu.edu'])->whereNotNull('room')->paginate(10);
     	return view('backend.allocated', [
     		'title' => 'Allocated Rooms',
     		'students' => $students
